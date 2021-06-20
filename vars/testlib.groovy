811acd92e8eabde6e2d8cb8769295b32058f8cc7
Oleksii Ryzhkov <oleksii_ryzhkov@epam.com>
2021-06-20 00:49:27 +0000

    Changed library

--- vars/testlib.groovy
+++ vars/testlib.groovy
@@ -3,9 +3,10 @@ def unit_function(UNIT_TESTS) {
     if ("${UNIT_TESTS}" == 'on') {
         sh "cat test123123123.file"
     }
+    }
     else {
         sh 'echo "testing echo"'
     }
 
-    }
+    
 }
