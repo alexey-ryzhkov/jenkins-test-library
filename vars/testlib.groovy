11ede4087cd7d0acffd2a99098c0c882a0c040b0
Oleksii Ryzhkov <oleksii_ryzhkov@epam.com>
2021-06-20 00:51:21 +0000

    Changed library

--- vars/testlib.groovy
+++ vars/testlib.groovy
@@ -1,12 +1,13 @@
 def unit_function(UNIT_TESTS) {
-    catchError(buildResult: 'Unstable', stageResult: 'FAILURE') {
+//    catchError(buildResult: 'Unstable', stageResult: 'FAILURE') {
     if ("${UNIT_TESTS}" == 'on') {
         sh "cat test123123123.file"
     }
-    }
+    
     else {
         sh 'echo "testing echo"'
     }
-
+    
+//    }
     
 }
