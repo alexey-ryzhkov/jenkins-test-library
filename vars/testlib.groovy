cdf2f8135032f5a900e790bbada39bc245e0f81c
Oleksii Ryzhkov <oleksii_ryzhkov@epam.com>
2021-06-20 00:48:26 +0000

    Changed library

--- vars/testlib.groovy
+++ vars/testlib.groovy
@@ -1,5 +1,5 @@
 def unit_function(UNIT_TESTS) {
-    
+    catchError(buildResult: 'Unstable', stageResult: 'FAILURE') {
     if ("${UNIT_TESTS}" == 'on') {
         sh "cat test123123123.file"
     }
@@ -7,4 +7,5 @@ def unit_function(UNIT_TESTS) {
         sh 'echo "testing echo"'
     }
 
+    }
 }
